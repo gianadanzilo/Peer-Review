676f31ac46a33dd1edd6886a2dae9b1281c61a2c
gianadanzilo <gdanzil@emory.edu>
2026-04-30 22:15:42 -0400

    add peer code and replication files

---
+++ data-code.R
@@ -0,0 +1,50 @@
+#load library packages
+library(pacman)
+p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, scales, kableExtra, modelsummary)
+getwd()
+setwd("/Users/tammydang/Downloads/EMORY/ECON-470")
+
+#load HCRIS dataset from 2009-2019
+dat.2009 <- read_csv("hmwk_data/data-2009.csv")
+dat.2010 <- read_csv("hmwk_data/data-2010.csv")
+dat.2011 <- read_csv("hmwk_data/data-2011.csv")
+dat.2012 <- read_csv("hmwk_data/data-2012.csv")
+dat.2013 <- read_csv("hmwk_data/data-2013.csv")
+dat.2014 <- read_csv("hmwk_data/data-2014.csv")
+dat.2015 <- read_csv("hmwk_data/data-2015.csv")
+dat.2016 <- read_csv("hmwk_data/data-2016.csv")
+dat.2017 <- read_csv("hmwk_data/data-2017.csv")
+dat.2018 <- read_csv("hmwk_data/data-2018.csv")
+dat.2019 <- read_csv("hmwk_data/data-2019.csv")
+
+#check overall colnames for each year to make sure data is right
+datasets <- list(
+  "2009" = dat.2009,
+  "2010" = dat.2010,
+  "2011" = dat.2011,
+  "2012" = dat.2012,
+  "2013" = dat.2013,
+  "2014" = dat.2014,
+  "2015" = dat.2015,
+  "2016" = dat.2016,
+  "2017" = dat.2017,
+  "2018" = dat.2018,
+  "2019" = dat.2019
+
+)
+colnames_list <- lapply(datasets, colnames)
+common_vars <- Reduce(intersect, colnames_list)
+common_vars #common variables across all years
+
+#variables unique to each year
+unique_vars <- lapply(colnames_list, function(x) {
+  setdiff(x, Reduce(intersect, colnames_list))
+})
+unique_vars #should be 0
+
+# combine all datasets
+na.full <- rbind(dat.2009, dat.2010, dat.2011, dat.2012, dat.2013, dat.2014, dat.2015, dat.2016, dat.2017, dat.2018, dat.2019)
+str(na.full)
+
+write_csv(na.full, "hmwk_data/hcris_data_2009-2019.csv")
+
